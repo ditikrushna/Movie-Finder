459e249de9a0c931f259d182f0de6b45df532185
diti.geek@gmail.com <diti.geek@gmail.com>
2021-03-15 02:12:06 +0530

    first-version

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  
+  return (
+    <footer className="page-footer">
+      <div className="text-center">
+      Handcrafted with ❤️ by Ditikrushna Giri 
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieInput from './MovieInput'
+const Navbar = () => {
+  return(
+    <header>
+      <nav id='pageTop' className='navbar'>
+        <h1 className='navbar__brand'>Movie Finder</h1>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
